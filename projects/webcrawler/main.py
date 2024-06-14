b9af41c050af20ec074a893b85d160cfd1d45d4a
ponomarenko <2863301+ponomarenko@users.noreply.github.com>
2024-06-14 18:22:42 +0300

    feat: add  `rich.table` for printing report

--- projects/webcrawler/main.py
+++ projects/webcrawler/main.py
@@ -1,8 +1,18 @@
+import os
 import re
 import requests
+import colorama
+from pathlib import Path
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
-import colorama
+from rich.console import Console
+from rich.table import Table
+
+int_ext_url_table = Table(title="Internal / External URLs")
+
+int_ext_url_table.add_column("Link", style="cyan", no_wrap=True)
+int_ext_url_table.add_column("Source", style="magenta")
+int_ext_url_table.add_column("Type", justify="right", style="green")
 
 # init the colorama module
 colorama.init()
@@ -12,77 +22,78 @@ GRAY = colorama.Fore.LIGHTBLACK_EX
 RESET = colorama.Fore.RESET
 YELLOW = colorama.Fore.YELLOW
 
+report_folder = "data"
+
 # initialize the set of links (unique links)
 internal_urls = set()
 external_urls = set()
 
 total_urls_visited = 0
 
+
 def is_valid(url):
-    """
-    Checks whether `url` is a valid URL.
-    """
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
-  
-def is_email(url):
-  """
-  Перевіряє, чи є URL-адреса дійсною електронною поштою.
 
-  Args:
-    url (str): URL-адреса для перевірки.
 
-  Returns:
-    bool: True, якщо URL-адреса є дійсною електронною поштою, False - інакше.
-  """
-
-  # Регулярний вираз для перевірки дійсності електронної пошти
-  email_regex = re.compile(r"[^@]+@[^@]+\.[^@]+")
-
-  # Перевірка, чи URL-адреса відповідає формату email
-  return email_regex.match(url) is not None
+def is_excluded_link(url):
+    """
+    Checks if the URL is an excluded type (email, javascript, etc.)
+    You can customize this function to add more exclusions.
+    """
+    excluded_patterns = [r"^mailto:", r"^javascript:", r"^tel:", r"^viber:"]
+    for pattern in excluded_patterns:
+        if re.match(pattern, url, re.IGNORECASE):
+            return True
+    return False
 
 
 def get_all_website_links(url):
-    """
-    Returns all URLs that is found on `url` in which it belongs to the same website
-    """
-    # all URLs of `url`
     urls = set()
     response = None
 
     try:
-      response = requests.get(url)
+        response = requests.get(url)
     except requests.exceptions.ConnectionError:
-      print(f"{GRAY}[!] Connection refused: {url}{RESET}")
-      return []
+        print(f"{GRAY}[!] Connection refused: {url}{RESET}")
+        return []
 
     soup = BeautifulSoup(response.content, "html.parser")
+
     for a_tag in soup.findAll("a"):
         href = a_tag.attrs.get("href")
+
         if href == "" or href is None:
-            # href empty tag
             continue
+
         # join the URL if it's relative (not absolute link)
         href = urljoin(url, href)
         parsed_href = urlparse(href)
+
         # remove URL GET parameters, URL fragments, etc.
         href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
-        if not is_valid(href) or is_email(href):
-            # not a valid URL
+
+        if not is_valid(href) or is_excluded_link(href):
             continue
+
         if href in internal_urls:
             # already in the set
             continue
+
         if domain_name not in href:
             # external link
             if href not in external_urls:
                 print(f"{GRAY}[!] External link: {href}{RESET}")
+                int_ext_url_table.add_row(href, url, "[yellow]External")
                 external_urls.add(href)
             continue
+
         print(f"{GREEN}[*] Internal link: {href}{RESET}")
+
         urls.add(href)
+
         internal_urls.add(href)
+        int_ext_url_table.add_row(href, url, "[green]Internal")
     return urls
 
 
@@ -105,15 +116,26 @@ def crawl(url, max_urls=30):
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser(description="Link Extractor Tool with Python")
     parser.add_argument("url", help="The URL to extract links from.")
-    parser.add_argument("-m", "--max-urls", help="Number of max URLs to crawl, default is 30.", default=30, type=int)
-    
+    parser.add_argument(
+        "-m",
+        "--max-urls",
+        help="Number of max URLs to crawl, default is 30.",
+        default=30,
+        type=int,
+    )
+
     args = parser.parse_args()
     url = args.url
     max_urls = args.max_urls
+
     # domain name of the URL without the protocol
     domain_name = urlparse(url).netloc
+
+    Path(report_folder).mkdir(parents=True, exist_ok=True)
+
     crawl(url, max_urls=max_urls)
 
     print("[+] Total Internal links:", len(internal_urls))
@@ -122,11 +144,22 @@ if __name__ == "__main__":
     print("[+] Total crawled URLs:", max_urls)
 
     # save the internal links to a file
-    with open(f"{domain_name}_internal_links.txt", "w", encoding="utf-8") as f:
+    with open(
+        os.path.join(report_folder, f"{domain_name}_internal_links.txt"),
+        "w",
+        encoding="utf-8",
+    ) as f:
         for internal_link in internal_urls:
             print(internal_link.strip(), file=f)
 
     # save the external links to a file
-    with open(f"{domain_name}_external_links.txt", "w", encoding="utf-8") as f:
+    with open(
+        os.path.join(report_folder, f"{domain_name}_external_links.txt"),
+        "w",
+        encoding="utf-8",
+    ) as f:
         for external_link in external_urls:
             print(external_link.strip(), file=f)
+
+    console = Console()
+    console.print(int_ext_url_table)
